2c4c9717321f003ba26c6b4623a8d86c6bec70ab
tjrongcai22 <tjrongcai_22@163.com>
2018-03-13 13:48:37 +0800

    修改路由2

--- src/router/index.js
+++ src/router/index.js
@@ -13,7 +13,8 @@ export default new Router({
     { path:'', component:Container },
     { path:'/login', component:Login },
     { path:'/container',component:Container},
-    { path:'*',redirect:'/container'}
+
+    // { path:'*',redirect:'/container'}
 
   ]
 
